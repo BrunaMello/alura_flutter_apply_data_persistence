1d4414ef0abdb4c17895b6be6b0447ffbabbb6fb
brunacrespomello <brunacrespomello@gmail.com>
2023-01-14 07:07:03 +0000

    03 - CRUD: read files
    We have already passed half the course and the rhythm is wonderful! In this class, we learned some really cool applications, let's review?
    
    CRUD: Database manipulation operations, CRUD is an acronym coming from English: Create, Read, Update, Delete - which translated means respectively, Create, Read, Update and Delete. We learned the concepts of CRUD and how they work in the sqflite package.
    
    Search Information: We started our implementation of CRUD by Read and saw how to implement the find() and findAll() functions that search for database information through the query() method available for Database-type objects, imported from the sqflite package.
    
    Transform Objects: We noticed the need to work with objects of type Map<String,dynamic> and of type List<Task> when we engage with the database. Thus, so that our project can communicate with the database, we learn to transform map-type objects - coming from the database - into Task List objects - used to build the screen.

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,4 +1,6 @@
 import 'package:nosso_primeiro_projeto/components/task.dart';
+import 'package:nosso_primeiro_projeto/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TaskDao {
   static const String tableSql = 'CREATE TABLE $_tablename('
@@ -11,12 +13,35 @@ class TaskDao {
   static const String _difficulty = 'difficulty';
   static const String _image = 'image';
 
-  save(Task task) async{}
+  save(Task task) async {}
 
-  Future<List<Task>> findAll() async{}
+  Future<List<Task>> findAll() async {
+    print('Assessing the Database:  ');
+    final Database database = await getDatabase();
+    final List<Map<String, dynamic>> result = await database.query(_tablename);
+    print('Searching data on database: $result');
+    return toList(result);
+  }
 
-  Future<List<Task>> find(String taskName) async{}
+  List<Task> toList(List<Map<String, dynamic>> mapTask) {
+    print('Converting to List');
+    final List<Task> tasks = [];
+    for (Map<String, dynamic> row in mapTask) {
+      final Task task = Task(row[_name], row[_image], row[_difficulty]);
+      tasks.add(task);
+    }
+    print('Task List: $tasks');
+    return tasks;
+  }
 
-  delete(String taskName) async{}
+  Future<List<Task>> find(String taskName) async {
+    print('Assessing find');
+    final Database database = await getDatabase();
+    final List<Map<String, dynamic>> result = await database
+        .query(_tablename, where: '$_name = ?', whereArgs: [taskName]);
+    print('Task found: ${toList(result)}');
+    return toList(result);
+  }
 
+  delete(String taskName) async {}
 }
